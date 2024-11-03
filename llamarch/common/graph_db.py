7969389237743079fb359e085756a9cf0f11086a
Pavan BG <gpbabc@yahoo.com>
2024-11-03 23:00:08 +0530

    Added neo4j support to common

--- llamarch/common/graph_db.py
+++ llamarch/common/graph_db.py
@@ -9,22 +9,16 @@ class GraphDB:
     def close(self):
         self.driver.close()
 
-    def add_node(self, label, properties):
+    def write_data(self, query, parameters=None):
         with self.driver.session() as session:
-            session.write_transaction(self._add_node, label, properties)
+            session.write_transaction(self._execute_query, query, parameters)
 
-    @staticmethod
-    def _add_node(tx, label, properties):
-        query = f"CREATE (n:{label} {{ {', '.join(f'{k}: ${k}' for k in properties)} }})"
-        tx.run(query, **properties)
-
-    def find_node(self, label, property_key, property_value):
+    def read_data(self, query, parameters=None):
         with self.driver.session() as session:
             result = session.read_transaction(
-                self._find_node, label, property_key, property_value)
-        return result
+                self._execute_query, query, parameters)
+            return result
 
     @staticmethod
-    def _find_node(tx, label, property_key, property_value):
-        query = f"MATCH (n:{label} {{ {property_key}: $value }}) RETURN n"
-        return tx.run(query, value=property_value).data()
+    def _execute_query(tx, query, parameters):
+        return tx.run(query, parameters)
