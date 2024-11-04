2ae51f2fdf53a2f4ea72c05a117bd5df93deba98
Pavan BG <gpbabc@yahoo.com>
2024-11-04 23:06:36 +0530

    Added neo4j docker instruction

--- llamarch/patterns/knowledge_graph/example.py
+++ llamarch/patterns/knowledge_graph/example.py
@@ -1,3 +1,7 @@
+# Start basic neo4j server using
+# docker run --name neo4j -p 7474:7474 -p 7687:7687 -d \
+#   -e NEO4J_AUTH=neo4j/testpassword neo4j:latest
+
 from llamarch.common.llm import LLM
 from llamarch.common.graph_db import GraphDB
 from llamarch.patterns.knowledge_graph import KnowledgeLLM
