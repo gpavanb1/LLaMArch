53752d383dd0b57d54e8d8c9c96e851f56b1091d
Pavan BG <gpbabc@yahoo.com>
2024-11-10 22:36:53 +0530

    Release version patterns

--- llamarch/patterns/multiplexing/aggregator.py
+++
@@ -1,4 +0,0 @@
-class OutputAggregator:
-    def aggregate(self, outputs):
-        # Simply concatenates outputs for now, but could apply more sophisticated aggregation logic
-        return " | ".join(outputs)
